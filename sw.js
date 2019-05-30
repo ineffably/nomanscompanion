da9e4ee981aa02989dd2ae2af9403b58b4925191
Chad Neff <ccneff@gmail.com>
2019-05-30 12:18:08 -0700

    testing sw updates

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
 self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('nomanscompanion').then(function (cache) {
-      return cache.addAll([
-        '/nomanscompanion/#/',
-        '/nomanscompanion/#/crafting',
-        '/nomanscompanion/#/refinement',
-        '/nomanscompanion/#/table',
-        '/nomanscompanion/dist/bundle.js',
-        '/nomanscompanion/icons/PRODUCT.CAVECRAFT.png',
-        '/nomanscompanion/icons/PRODUCT.UPGRADE.SHIP.png'
-      ]);
-    })
-  );
+  if (caches) {
+    e.waitUntil(
+      caches.open('nomanscompanion').then(function (cache) {
+        return cache.addAll([
+          '/nomanscompanion/#/',
+          '/nomanscompanion/#/crafting',
+          '/nomanscompanion/#/refinement',
+          '/nomanscompanion/#/table',
+          '/nomanscompanion/dist/bundle.js',
+          '/nomanscompanion/icons/PRODUCT.CAVECRAFT.png',
+          '/nomanscompanion/icons/PRODUCT.UPGRADE.SHIP.png'
+        ]).then(() => self.skipWaiting());
+      }, (e) => {console.log(e);})
+    );
+  }
 });
+
