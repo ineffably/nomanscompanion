dc1083699327772efc4321b23e07753743bba998
Chad Neff <ccneff@gmail.com>
2019-05-30 15:05:49 -0700

    sw-tweaks

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 self.addEventListener('install', function (e) {
+  console.log('==> sw.install');
   if (caches) {
     e.waitUntil(
       caches.open('nomanscompanion').then(function (cache) {
-        return cache.addAll([
+        const allCache = cache.addAll([
           '/nomanscompanion/#/',
           '/nomanscompanion/#/crafting',
           '/nomanscompanion/#/refinement',
@@ -11,7 +12,8 @@ self.addEventListener('install', function (e) {
           '/nomanscompanion/icons/PRODUCT.CAVECRAFT.png',
           '/nomanscompanion/icons/PRODUCT.UPGRADE.SHIP.png'
         ]).then(() => self.skipWaiting());
-      }, (e) => {console.log(e);})
+        return allCache;
+      }, (e) => { console.log(e); })
     );
   }
 });
