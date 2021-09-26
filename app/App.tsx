bcab2e23196a07772c02a4213028cfac26e53422
Chad Neff <ccneff@gmail.com>
2021-09-26 16:30:19 -0600

    removed langauge.json from the build

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React, { useEffect, FunctionComponent, useState } from 'react';
 import { GCProductEntry, DebrisFile } from '../types/gcproduct';
-// import { ProductTable } from './ProductTable';
 import { MinProductCard } from './MinProductCard';
 import './App.css';
-import languageTokens from '../assets/data/all_usenglish.json';
 import { colorFromEntry, getIconUrlFromDebrisFile } from './utils';
 
-const transformProductTable = (entries: GCProductEntry[]) => {
+const transformProductTable = (entries: GCProductEntry[], languageTokens: Record<string, string>) => {
   return entries.map(row => {
     row.Name = languageTokens[row.Name] || row.Name;
     row.NameLower = languageTokens[row.NameLower] || row.NameLower;
@@ -26,7 +24,8 @@ const transformProductTable = (entries: GCProductEntry[]) => {
 
 export const App: FunctionComponent = () => {
   const [productTable, setProductTable] = useState([] as GCProductEntry[])
-
+  const [languageTokens, setLanguageTokens] = useState({} as Record<string, string>);
+  
   const fetchDataAsset = async (location, onSuccess = json => { }) => {
     const response = await window.fetch(location);
     const json = await response.json();
@@ -36,17 +35,25 @@ export const App: FunctionComponent = () => {
   }
 
   const fetchData = async () => {
-    const incomingProductTable = [];
-    fetchDataAsset('/assets/data/nms_reality_gctechnologytable.json', ({ Table }: { Table: GCProductEntry[] }) => {
-      incomingProductTable.push(transformProductTable(Table));
-      fetchDataAsset('/assets/data/nms_u3reality_gcproducttable.json', ({ Table }: { Table: GCProductEntry[] }) => {
-        incomingProductTable.push(transformProductTable(Table));
-        fetchDataAsset('/assets/data/nms_reality_gcsubstancetable.json', ({ Table }: { Table: GCProductEntry[] }) => {
-          incomingProductTable.push(transformProductTable(Table));
-          setProductTable(incomingProductTable.flat());
+    const fetchProductTables = (tokens: Record<string, string>) => {
+      const incomingProductTable = [];
+      fetchDataAsset('/assets/data/nms_reality_gctechnologytable.json', ({ Table }: { Table: GCProductEntry[] }) => {
+        incomingProductTable.push(transformProductTable(Table, tokens));
+        fetchDataAsset('/assets/data/nms_u3reality_gcproducttable.json', ({ Table }: { Table: GCProductEntry[] }) => {
+          incomingProductTable.push(transformProductTable(Table, tokens));
+          fetchDataAsset('/assets/data/nms_reality_gcsubstancetable.json', ({ Table }: { Table: GCProductEntry[] }) => {
+            incomingProductTable.push(transformProductTable(Table, tokens));
+            setProductTable(incomingProductTable.flat());
+          });
         });
       });
-    });
+    }
+
+    fetchDataAsset('/assets/data/all_usenglish.json', (language) => {
+      setLanguageTokens(language);
+      fetchProductTables(language);
+    })
+
   }
 
   useEffect(() => {
